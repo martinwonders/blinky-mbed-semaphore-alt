d4250c914018308407e916d59460d06be45dac0d
martinwonders <martin.wonders@unn.ac.uk>
2019-11-09 05:01:00 -0800

    completed 2.2 resource of 1 semaphore

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,16 @@
 
 enum{ON,OFF}; /* a hack to invert bit logic for common anode LED */
 
-//Semaphore flag(0);
+Semaphore flag(1);
 
-void flash_red(DigitalOut *led)
+void flash_led(DigitalOut *led)
 {
 	wait_ms( rand()%1000 );
 	while(true) {
-//		flag.wait();
+		flag.wait();
 		*led = ON;
 		wait_ms( rand()%1000 );
-//		flag.release();
+		flag.release();
 		*led = OFF;
 		wait_ms( rand()%1000 );
 	}
@@ -19,15 +19,20 @@ void flash_red(DigitalOut *led)
 
 int main(void)
 {
-	DigitalOut   red(LED1,OFF);
+	DigitalOut red(LED1,OFF);
 	DigitalOut green(LED2,OFF);
-	DigitalOut  blue(LED3,OFF);
-	Thread p,q,r;
+	DigitalOut blue(LED3,OFF);
+        DigitalOut red_as(D5,OFF);
+	DigitalOut green_as(D8,OFF);
+	DigitalOut blue_as(D9,OFF);
+	Thread p,q,r,s,t,u;
 
-	r.start( callback(flash_red, &red) );
-	p.start( callback(flash_red, &green) );
-	q.start( callback(flash_red, &blue) );
-
+	r.start( callback(flash_led, &red) );
+	p.start( callback(flash_led, &green) );
+	q.start( callback(flash_led, &blue) );
+        s.start( callback(flash_led, &red_as) );
+	t.start( callback(flash_led, &green_as) );
+	u.start( callback(flash_led, &blue_as) );
 	r.join();	
 }
 
